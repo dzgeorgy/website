d250133c06a6304516a7830d6258e2ffa739914a
dzgeorgy <dzgeorgy@gmail.com>
2020-04-22 02:49:10 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
+import './style.scss';
+
+const feather = require('feather-icons');
+feather.replace();
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+    entry: './app.js',
+    output: {
+        path: path.resolve(__dirname, 'docs'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(scss)$/,
+                use: [{
+                    loader: 'style-loader',
+                }, {
+                    loader: 'css-loader',
+                }, {
+                    loader: 'postcss-loader',
+                    options: {
+                        plugins: function () {
+                            return [
+                                require('autoprefixer')
+                            ];
+                        }
+                    }
+                }, {
+                    loader: 'sass-loader'
+                }]
+            }
+        ]
+    }
+};
